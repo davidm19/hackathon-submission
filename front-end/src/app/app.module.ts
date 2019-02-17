81053e7700a76cf2bb06451d4f75381508cb26cd
miadimson2019 <miadimson2019@chadwickschool.org>
2019-02-16 16:11:59 -0800

    Created preliminary front end website with a template. No functionality yet

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,16 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateHikeComponent } from './components/hike/create-hike/create-hike.component';
+import { HikeComponent } from './components/hike/hike.component';
+import { SidebarMenuComponent } from './components/sidebar-menu/sidebar-menu.component';
+import { TripsComponent } from './components/trips/trips.component';
+
+
+const appRoutes: Routes = [
+  { path: '', component: TripsComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateHikeComponent,
+    HikeComponent,
+    SidebarMenuComponent,
+    TripsComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
